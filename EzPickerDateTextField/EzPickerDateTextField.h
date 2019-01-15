38a8023d8af9e72c1be5db6860f174c4a90dd4bf
Brent Michalski <Brent.Michalski@gmail.com>
2019-01-15 17:41:30 -0600

    Initial Commit

---
+++ EzPickerDateTextField/EzPickerDateTextField.h
@@ -0,0 +1,19 @@
+//
+//  EzPickerDateTextField.h
+//  EzPickerDateTextField
+//
+//  Created by Brent Michalski on 1/15/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EzPickerDateTextField.
+FOUNDATION_EXPORT double EzPickerDateTextFieldVersionNumber;
+
+//! Project version string for EzPickerDateTextField.
+FOUNDATION_EXPORT const unsigned char EzPickerDateTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EzPickerDateTextField/PublicHeader.h>
+
+
